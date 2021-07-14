8f21cbdaa08e1f2d4343662b1ac86fbc97db6093
Mary Morvan <mpmorvan@talbotdrake.com>
2021-07-14 16:27:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -19,18 +17,14 @@ const sess = {
     db: sequelize
   })
 };
-
 app.use(session(sess));
-
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(require('./controllers/'));
 
 app.listen(PORT, () => {
