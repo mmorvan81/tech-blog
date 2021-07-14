2b4680bec992833c97d803beaf4479f0f149fbae
Mary Morvan <mpmorvan@talbotdrake.com>
2021-07-14 16:22:39 -0400

    Update dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,18 +1,13 @@
 const router = require('express').Router();
 const { Post } = require('../models/');
 const withAuth = require('../utils/auth');
-
 router.get('/', withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      where: {
-        userId: req.session.userId,
+  try { const postData = await Post.findAll({
+      where: { userId: req.session.userId,
       },
     });
-
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    res.render('all-posts-admin', {
+const posts = postData.map((post) => post.get({ plain: true }));
+ res.render('all-posts-admin', {
       layout: 'dashboard',
       posts,
     });
@@ -20,20 +15,16 @@ router.get('/', withAuth, async (req, res) => {
     res.redirect('login');
   }
 });
-
 router.get('/new', withAuth, (req, res) => {
   res.render('new-post', {
     layout: 'dashboard',
   });
 });
-
 router.get('/edit/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id);
-
     if (postData) {
       const post = postData.get({ plain: true });
-
       res.render('edit-post', {
         layout: 'dashboard',
         post,
@@ -45,5 +36,4 @@ router.get('/edit/:id', withAuth, async (req, res) => {
     res.redirect('login');
   }
 });
-
 module.exports = router;
