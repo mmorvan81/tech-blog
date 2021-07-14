3e01bb49fb18babbebb8a6268024c4b6da74a35b
Mary Morvan <mpmorvan@talbotdrake.com>
2021-07-14 16:26:37 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/config');
-
-// create our User model
 class User extends Model {
-  // set up method to run on instance data (per user) to check password
+
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
-  }
-}
-
+  }}
 User.init(
   {
     id: {
@@ -27,12 +23,10 @@ User.init(
       allowNull: false,
       validate: {
         len: [4]
-      }
-    }
+      }}
   },
   {
     hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
@@ -40,8 +34,7 @@ User.init(
       beforeUpdate: async (updatedUserData) => {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
-      }
-    },
+      }},
     sequelize,
     timestamps: false,
     freezeTableName: true,
@@ -49,5 +42,4 @@ User.init(
     modelName: 'User'
   }
 );
-
 module.exports = User;
