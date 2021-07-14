3a63e8ef1f983149b024fb53aa26fe1f52f8bee7
Mary Morvan <mpmorvan@talbotdrake.com>
2021-07-14 16:23:50 -0400

    Update home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,22 +1,16 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models/');
 
-// get all posts for homepage
 router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
       include: [User],
     });
-
     const posts = postData.map((post) => post.get({ plain: true }));
-
     res.render('all-posts', { posts });
   } catch (err) {
     res.status(500).json(err);
-  }
-});
-
-// get single post
+  }});
 router.get('/post/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
@@ -28,7 +22,6 @@ router.get('/post/:id', async (req, res) => {
         },
       ],
     });
-
     if (postData) {
       const post = postData.get({ plain: true });
 
@@ -40,23 +33,18 @@ router.get('/post/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
-
   res.render('login');
 });
-
 router.get('/signup', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
-
   res.render('signup');
 });
-
 module.exports = router;
